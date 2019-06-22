27b87a4c8fa69cde753dab3fba7140306ca56dba
Anwesh43 <anweshthecool0@gmail.com>
2019-06-22 14:26:18 +0530

    adding constants

--- doublelinetosquareview/src/main/java/com/anwesh/uiprojects/doublelinetosquareview/DoubleLineToSquareView.kt
+++ doublelinetosquareview/src/main/java/com/anwesh/uiprojects/doublelinetosquareview/DoubleLineToSquareView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.doublelinetosquareview
 /**
  * Created by anweshmishra on 22/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#009688")
+val backColor : Int = Color.parseColor("#BDBDBD")
