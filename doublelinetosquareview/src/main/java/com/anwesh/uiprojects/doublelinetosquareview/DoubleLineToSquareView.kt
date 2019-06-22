e8de0730b4b51009c834cfe17c7c1ade36ab049d
Anwesh43 <anweshthecool0@gmail.com>
2019-06-22 15:38:43 +0530

    adding Canvas extension methods

--- doublelinetosquareview/src/main/java/com/anwesh/uiprojects/doublelinetosquareview/DoubleLineToSquareView.kt
+++ doublelinetosquareview/src/main/java/com/anwesh/uiprojects/doublelinetosquareview/DoubleLineToSquareView.kt
@@ -30,3 +30,43 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawRotLine(i : Int, size : Float, scale : Float, paint : Paint) {
+    save()
+    translate((size / 2) * (1f - 2 * i), 0f)
+    drawLine(0f, 0f, 0f, size / 2, paint)
+    save()
+    translate(0f, size / 2)
+    rotate(90f * (1f - 2 * i) * scale.divideScale(i, lines / 2))
+    drawLine(0f, 0f, 0f, size / 2, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawDoubleRotLine(i : Int, size : Float, scale : Float, paint : Paint) {
+    val sci : Float = scale.divideScale(i, lines / 2)
+    for (j in 0..(lines / 2 - 1)) {
+        save()
+        drawRotLine(j, size, sci, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawDLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, gap * (i + 1))
+    scale(sc2, sc2)
+    for (j in 0..(lines / 2 - 1)) {
+        drawDoubleRotLine(j, size, sc1, paint)
+    }
+    restore()
+}
